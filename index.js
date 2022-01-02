2c38575b961b2d26d54a278759b3ac8ec00e42da
fredrikb12 <fredrik.backman12@gmail.com>
2022-01-02 17:22:40 +0100

    Remove unnecessary code and clean up index.js

--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 const container = document.querySelector(".container");
-/*for(let i = 0; i < 16; i++) {
-    let newColumn = document.createElement("div");
-    newColumn.classList.add("column");
-    for(let j = 0; j < 16; j++) {
-        let newSquare = document.createElement("div");
-        newSquare.classList.add("grid-square");
-        newSquare.textContent = "div";
-        newColumn.append(newSquare);
-    }
-    container.appendChild(newColumn);
-}*/
 
 function createGrid(numberOfDivs) {
     if(typeof numberOfDivs !== "number") {
